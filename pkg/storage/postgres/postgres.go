e029def4654cea7498cb53c80e96abfbfa27c2e0
leejoys <44557202+leejoys@users.noreply.github.com>
2021-06-04 17:41:52 +0300

    Posts() ready

--- pkg/storage/postgres/postgres.go
+++ pkg/storage/postgres/postgres.go
@@ -26,11 +26,45 @@ func New() *Store {
 }
 
 func (s *Store) Posts() ([]storage.Post, error) {
+	rows, err := s.db.Query(context.Background(),
+		`SELECT 
+	posts.id, 
+	posts.title, 
+	posts.content, 
+	posts.author_id,
+	author.name, 
+	posts.create_at 
+	FROM posts
+	JOIN authors
+	AT authors.name=posts.author_id;`)
+
+	if err != nil {
+		return nil, err
+	}
 	var posts []storage.Post
-	return posts, nil
+	for rows.Next() {
+		var p storage.Post
+		err = rows.Scan(
+			&p.ID,
+			&p.Title,
+			&p.Content,
+			&p.AuthorID,
+			&p.AuthorName,
+			&p.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, p)
+	}
+
+	return posts, rows.Err()
 }
 
-func (s *Store) AddPost(storage.Post) error {
+func (s *Store) AddPost(p storage.Post) error {
+	err := s.db.QueryRow(SELECT)
+
+	err = s.db.Exec("INSERT INTO posts ", p.Title, p.Content, p.AuthorID, p.CreatedAt)
 	return nil
 }
 func (s *Store) UpdatePost(storage.Post) error {
