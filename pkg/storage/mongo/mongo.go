9bb85c2a735504aeac90660f9e588f2d132c7f31
leejoys <44557202+leejoys@users.noreply.github.com>
2021-06-22 02:10:16 +0300

    mongo posts

--- pkg/storage/mongo/mongo.go
+++ pkg/storage/mongo/mongo.go
@@ -4,27 +4,55 @@ import (
 	"GoNews/pkg/storage"
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Хранилище данных.
 type Store struct {
-	db *mongo.Client
+	c          *mongo.Client
+	db         string
+	collection string
 }
 
 // Конструктор объекта хранилища.
 func New(connstr string) (*Store, error) {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connstr))
+	client, err := mongo.Connect(context.Background(),
+		options.Client().ApplyURI(connstr))
 	if err != nil {
 		return nil, err
 	}
-	return &Store{db: client}, nil
+	// проверка связи с БД
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		client.Disconnect(context.Background())
+		return nil, err
+	}
+	return &Store{c: client, db: "data",
+		collection: "posts"}, nil
 }
 
 func (s *Store) Posts() ([]storage.Post, error) {
-	//err := s.db.Database() // <--
+
+	coll := s.c.Database(s.db).Collection(s.collection)
+	ctx := context.Background()
+	filter := bson.D{}
+	cur, err := coll.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(ctx)
+
 	var posts []storage.Post
+	for cur.Next(ctx) {
+		var p storage.Post
+		err = cur.Decode(&p)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, p)
+	}
 	return posts, nil
 }
 
